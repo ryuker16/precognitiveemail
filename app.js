507060fd30e08214ab4fa602383d17c6ddc7cb4b
ryuker16 <aeoneon@gmail.com>
2017-10-24 15:42:05 -0500

    updated comments in app.js

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const dbName = config.get('dbName');
 
 //original device opened
 let originalDevice = '';
-
 // we should be not be caching our image since we want tracking everytime email is opened
 app.use(nocache());
 // capture user device
@@ -24,9 +23,10 @@ app.use(device.capture());
 app.listen(5100);
 
 // our middleware intercepts any request to check if it has 
-// tracking pxl query params and puts relevant device data and tracking code into databae
+// tracking pxl query params and puts relevant device data into database to relevant tracking pixel doc
 app.use((req, res, next) => {
     console.log('request coming through');
+    // check for trackking pixel
     if (req.query.pxl) {
         console.log(req.query.pxl);
         let setDetails = {
@@ -34,6 +34,7 @@ app.use((req, res, next) => {
             'device': req.device.type,
             'timeStamp': new Date()
         };
+        // connect to mongdb and update data for tracking pixel doc
         mongo.connect(`mongodb://${dbUrl}:${dbPort}/${dbName}`, (err, db) => {
             if (err) {
                 console.log(err);
@@ -52,21 +53,26 @@ app.use((req, res, next) => {
                     if (err) {
                         console.log(err);
                     }
-
+                    // check if original device has been set, originally
+                    // did this via mongoDb with $setOnInsert but it did not work
+                    // when doc was already inserted during get tracking pixel
                     if (originalDevice === '') {
                         originalDevice = req.device.type;
                         doc.value.originalDevice = req.device.type;
                     }
-
-                    if (originalDevice !== req.device.type) {
+                    // if device changed, set status which we will return to user and console.log
+                    if (originalDevice !== req.device.type ) {
                         doc.value.deviceChangedOnReOpen = true;
                     } else {
                         doc.value.deviceChangedOnReOpen = false;
                     }
                     // This is the part where we would send email but instead log out data
                     let allData = Object.assign(doc.value, setDetails);
+                    // log data out to server in lieu of email
                     console.log(allData);
+                    // close connection
                     db.close();
+                    // send all relevant and requested data back to user about tracking
                     res.send(allData);
                 }));
              
@@ -74,6 +80,7 @@ app.use((req, res, next) => {
         });
 
     } else {
+        // no tracking pxl found in query params so continue. 
         next();
     }
 });
@@ -93,7 +100,7 @@ app.get('/gettracker/:email', (req, res) => {
                 'length': 5
             })
         };
-
+        // conect to db and insert the metaData object then send response string with tracking pixel
         mongo.connect(`mongodb://${dbUrl}:${dbPort}/${dbName}`, (err, db) => {
             db.collection('precogdocs').insert(metaData).then(() => {
                     res.set('Content-Type', 'text/plain');
